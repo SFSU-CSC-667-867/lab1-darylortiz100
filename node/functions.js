443133426e1d7836f62dc8101c50e419b051b9a3
darylortiz100 <34200876+darylortiz100@users.noreply.github.com>
2019-02-06 20:27:50 -0800

    lab1 practicing functions.js

--- node/functions.js
+++ node/functions.js
@@ -14,6 +14,16 @@ arrow('hi')
 
 // for practice
 
-// make an arrow function with no block body
+// make an arrow function with no block bodyy
+const myArrow = (y) => y + 1;
+//{
+//  return y+1;
+//}
+console.log(myArrow(8));
 
 // make a function that returns a function
+const functionFactory = () => (-1) => console.log('This is weird');
+const a = funtionFactory();
+a();
+const anotherFunction = functionFactory;
+anotherFunction()();
